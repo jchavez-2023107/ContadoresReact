b5ce34ff57e1f2af610c73767294a93b893dba18
clarios-2020611 <clarios-2020611@kinal.edu.gt>
2025-04-07 15:58:55 -0600

    Ejercicio de suma de precios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,33 @@ export const Home = () => {
     const [count, setCount] = useState(0)
     const [buying, setBuying] = useState(false)
     const [total, setTotal] = useState(0)
+    const [products, setProducts] = useState([])
 
-    const buy = () => {
+    const buy = (price) => {
         setBuying(true)
-        alert('Gracias por tu compra')
+        alert('Gracias por tu compra');
+
+        //Aquí es donde actualizamos el contador de ventas y el total vendido
+        setCount(prevCount => prevCount+1)
+        setTotal(prevTotal => prevTotal + price)
+    }
+
+    const getUsers = async ()=>{
+        //Consultar al API
+        //1ra forma utilizando Fetch (Puentes de conexión entre Front y Back)
+        const response = await fetch('https://fakestoreapi.com/products/')
+        const results = await response.json()
+        setProducts(results)
+        
+
+        //2da forma utilizando Axios
     }
 
     useEffect(() => {
     //Creando (Lógica)
     //Si la variable buying cambia se ejecuta este código
     if (buying){
-        setCount(count + 1);
-        setTotal(total + 1800)
+        console.log("Se hizo la compra");
     } else {
         console.log('No hay compras');
     }
@@ -28,7 +43,12 @@ export const Home = () => {
     }, [buying]) //Dependencia de actualización
         //Arreglo de dependencias
 
+
     useEffect(() => { //Efecto secundario
+        //Consultar al API
+        getUsers();
+
+
         alert('Cargando datos desde la BD')
         setTimeout(() => {
             //En la BD ya hay 500 ventas
@@ -40,7 +60,10 @@ export const Home = () => {
     <div>
         <span>Ventas totales: {count}</span>
         <br />
-        <span>Total vendido: {total}</span>
+        <span>Total vendido: Q{total}</span>
+        <br /><br />
+        <button>Obtener más productos</button>
+        <br /><br />
         <h2>Contador de ventas.</h2>
         <table>
             <thead>
@@ -54,18 +77,31 @@ export const Home = () => {
                 <tr>
                     <td>Zapatos</td>
                     <td>1500</td>
-                    <td><button onClick={buy}>Comprar</button></td>
+                    <td><button onClick={() => buy(1500)}>Comprar</button></td>
                 </tr>
                 <tr>
                     <td>Playera</td>
                     <td>800</td>
-                    <td><button onClick={buy}>Comprar</button></td>
+                    <td><button onClick={() => buy(800)}>Comprar</button></td>
                 </tr>
                 <tr>
                     <td>Laptop</td>
                     <td>5000</td>
-                    <td><button onClick={buy}>Comprar</button></td>
+                    <td><button onClick={() => buy(5000)}>Comprar</button></td>
                 </tr>
+                {
+                    /* For: solamente itera */
+                    /* Map: Itera y genera un arreglo en base a las iteraciones */
+                    products.map((product)=> (
+                        <>
+                            <tr>
+                                <td>{product.title}</td>
+                                <td>{product.price}</td>
+                                <td><button onClick={() => buy(product.price)}> Comprar </button></td>
+                            </tr>
+                        </>
+                    ))
+                }
             </tbody>
         </table>
         {/* <div>Home</div>
